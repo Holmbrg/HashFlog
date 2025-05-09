d7c87abd1731de75f3722a58bee4d94f6dfd8158
Alexander Hansen <164519860+Holmbrg@users.noreply.github.com>
2025-05-09 11:10:13 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,9 +5,8 @@ Initializes modules and sub-packages in the
 repository 'HashFlog' on GitHub.
 """
 
-import data
 import HashFlog
 import main_login
 import main_register
 
-__all__ = ["data", "HashFlog", "main_login", "main_register"]
+__all__ = ["HashFlog", "main_login", "main_register"]
